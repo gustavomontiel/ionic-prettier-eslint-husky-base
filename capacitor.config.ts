79d57f95701a476fab64eb225e89569ccffe1cc3
Gustavo Montiel <gustavomontiel@gmail.com>
2024-04-17 18:29:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-prettier-eslint-husky-base',
+  webDir: 'www'
+};
+
+export default config;
